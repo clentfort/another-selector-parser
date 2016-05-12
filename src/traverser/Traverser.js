7dea5768f9186cd0902e496013e57c0e4cc832ba
Christian Lentfort <unkieee@gmail.com>
2016-05-12 22:23:19 +0200

    Roll out traverser for known AST nodes and add more tests

--- src/traverser/Traverser.js
+++ src/traverser/Traverser.js
@@ -11,22 +11,14 @@ export type VisitFunction = (node: nodes.Node) => void;
 export type Visitors = {
   [key: NodeType]: VisitFunction;
 };
-
 type ShouldVisitMap = {[key: NodeType]: ShouldVisit};
-const defaultShouldVisitMap = Object.keys(nodes).reduce(
-  (result: ShouldVisitMap, nodeType: string): ShouldVisitMap => {
-    result[nodeType] = generateShouldVisitForNodeT(nodes[nodeType]);
-    return result;
-  },
-  {}
-);
 
 export default class Visitor {
   _shouldVisitMap: ShouldVisitMap;
   _visitors: Visitors;
 
   constructor(visitors: Visitors) {
-    this._shouldVisitMap = { ...defaultShouldVisitMap };
+    this._shouldVisitMap = {};
     this._visitors = visitors;
   }
 
@@ -37,6 +29,149 @@ export default class Visitor {
   }
 
   visit(node: nodes.Node): void {
+    /* eslint-disable new-cap */
+    if (!!this._visitors.Node) {
+      this._visitors.Node(node);
+    }
+
+    if (
+      !!this._visitors.AttributeSelector &&
+      node instanceof nodes.AttributeSelector
+    ) {
+      this._visitors.AttributeSelector(node);
+    }
+
+    if (
+      !!this._visitors.AttributeSelectorAttribute &&
+      node instanceof nodes.AttributeSelectorAttribute
+    ) {
+      this._visitors.AttributeSelectorAttribute(node);
+    }
+
+    if (
+      !!this._visitors.AttributeSelectorMatcher &&
+      node instanceof nodes.AttributeSelectorMatcher
+    ) {
+      this._visitors.AttributeSelectorMatcher(node);
+    }
+
+    if (
+      !!this._visitors.AttributeSelectorValue &&
+      node instanceof nodes.AttributeSelectorValue
+    ) {
+      this._visitors.AttributeSelectorValue(node);
+    }
+
+    if (
+      !!this._visitors.AttributeSelectorWithMatcher &&
+      node instanceof nodes.AttributeSelectorWithMatcher
+    ) {
+      this._visitors.AttributeSelectorWithMatcher(node);
+    }
+
+    if (!!this._visitors.ClassSelector && node instanceof nodes.ClassSelector) {
+      this._visitors.ClassSelector(node);
+    }
+
+    if (!!this._visitors.Combinator && node instanceof nodes.Combinator) {
+      this._visitors.Combinator(node);
+    }
+
+    if (!!this._visitors.HashSelector && node instanceof nodes.HashSelector) {
+      this._visitors.HashSelector(node);
+    }
+
+    if (!!this._visitors.Identifier && node instanceof nodes.Identifier) {
+      this._visitors.Identifier(node);
+    }
+
+    if (!!this._visitors.Literal && node instanceof nodes.Literal) {
+      this._visitors.Literal(node);
+    }
+
+    if (
+      !!this._visitors.NamespacePrefix &&
+      node instanceof nodes.NamespacePrefix
+    ) {
+      this._visitors.NamespacePrefix(node);
+    }
+
+    if (!!this._visitors.NumberLiteral && node instanceof nodes.NumberLiteral) {
+      this._visitors.NumberLiteral(node);
+    }
+
+    if (
+      !!this._visitors.PseudoSelector &&
+      node instanceof nodes.PseudoSelector
+    ) {
+      this._visitors.PseudoSelector(node);
+    }
+
+    if (
+      !!this._visitors.CallExpression &&
+      node instanceof nodes.CallExpression
+    ) {
+      this._visitors.CallExpression(node);
+    }
+
+    if (
+      !!this._visitors.PseudoClassSelector &&
+      node instanceof nodes.PseudoClassSelector
+    ) {
+      this._visitors.PseudoClassSelector(node);
+    }
+
+    if (
+      !!this._visitors.PseudoElementSelector &&
+      node instanceof nodes.PseudoElementSelector
+    ) {
+      this._visitors.PseudoElementSelector(node);
+    }
+
+    if (!!this._visitors.Selector && node instanceof nodes.Selector) {
+      this._visitors.Selector(node);
+    }
+
+    if (!!this._visitors.SelectorList && node instanceof nodes.SelectorList) {
+      this._visitors.SelectorList(node);
+    }
+
+    if (
+      !!this._visitors.SimpleSelector &&
+      node instanceof nodes.SimpleSelector
+    ) {
+      this._visitors.SimpleSelector(node);
+    }
+
+    if (
+      !!this._visitors.SimpleSelectorList &&
+      node instanceof nodes.SimpleSelectorList
+    ) {
+      this._visitors.SimpleSelectorList(node);
+    }
+
+    if (
+      !!this._visitors.StringLiteral &&
+      node instanceof nodes.StringLiteral
+    ) {
+      this._visitors.StringLiteral(node);
+    }
+
+    if (
+      !!this._visitors.TypeSelector &&
+      node instanceof nodes.TypeSelector
+    ) {
+      this._visitors.TypeSelector(node);
+    }
+
+    if (
+      !!this._visitors.UniversalSelector &&
+      node instanceof nodes.UniversalSelector
+    ) {
+      this._visitors.UniversalSelector(node);
+    }
+    /* eslint-enable new-cap */
+
     Object.keys(this._shouldVisitMap).forEach(nodeType => {
       const shouldVisit = this._shouldVisitMap[nodeType];
       if (!!this._visitors[nodeType] && shouldVisit(node)) {
--- src/traverser/__tests__/Traverser-test.js
+++ src/traverser/__tests__/Traverser-test.js
@@ -207,4 +207,271 @@ describe('Traverser', () => {
     expect(calls[42][0]).toBe(typeSelector2);
     expect(calls[43][0]).toBe(typeSelector2.value);
   });
+
+  it('calls SelectorList for each SelectorList-Node', () => {
+    const selectorListVisitor = jest.fn();
+    const traverser = new Traverser({ SelectorList: selectorListVisitor });
+    traverser.visit(AST); 
+    const { calls } = selectorListVisitor.mock;
+    expect(calls.length).toBe(2);
+    expect(calls[0][0]).toBe(selectorList1);
+    expect(calls[1][0]).toBe(selectorList2);
+  });
+
+  it('calls Selector for each Selector-Node', () => {
+    const selectorVisitor = jest.fn();
+    const traverser = new Traverser({ Selector: selectorVisitor });
+    traverser.visit(AST); 
+    const { calls } = selectorVisitor.mock;
+    expect(calls.length).toBe(3);
+    expect(calls[0][0]).toBe(selector1);
+    expect(calls[1][0]).toBe(selector2);
+    expect(calls[2][0]).toBe(selector3);
+  });
+
+  it('calls `AttributeSelector` for each `AttributeSelector`-node', () => {
+    const attributeSelectorVisitor = jest.fn();
+    const traverser = new Traverser({ AttributeSelector: attributeSelectorVisitor });
+    traverser.visit(AST); 
+    const { calls } = attributeSelectorVisitor.mock;
+    expect(calls.length).toBe(2);
+    expect(calls[0][0]).toBe(attributeSelector1);
+    expect(calls[1][0]).toBe(attributeSelectorWithMatcher1);
+  });
+
+  it('calls `AttributeSelectorAttribute` for each `AttributeSelectorAttribute`-node', () => {
+    const attributeSelectorAttributeVisitor = jest.fn();
+    const traverser = new Traverser({ AttributeSelectorAttribute: attributeSelectorAttributeVisitor });
+    traverser.visit(AST); 
+    const { calls } = attributeSelectorAttributeVisitor.mock;
+    expect(calls.length).toBe(2);
+    expect(calls[0][0]).toBe(attributeSelector1.attribute);
+    expect(calls[1][0]).toBe(attributeSelectorWithMatcher1.attribute);
+  });
+
+  it('calls `AttributeSelectorMatcher` for each `AttributeSelectorMatcher`-node', () => {
+    const attributeSelectorMatcherVisitor = jest.fn();
+    const traverser = new Traverser({ AttributeSelectorMatcher: attributeSelectorMatcherVisitor });
+    traverser.visit(AST); 
+    const { calls } = attributeSelectorMatcherVisitor.mock;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toBe(attributeSelectorWithMatcher1.matcher);
+  });
+
+  it('calls `AttributeSelectorValue` for each `AttributeSelectorValue`-node', () => {
+    const attributeSelectorValueVisitor = jest.fn();
+    const traverser = new Traverser({ AttributeSelectorValue: attributeSelectorValueVisitor });
+    traverser.visit(AST); 
+    const { calls } = attributeSelectorValueVisitor.mock;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toBe(attributeSelectorWithMatcher1.value);
+  });
+
+  it('calls `AttributeSelectorWithMatcher` for each `AttributeSelectorWithMatcher`-node', () => {
+    const attributeSelectorWithMatcherVisitor = jest.fn();
+    const traverser = new Traverser({ AttributeSelectorWithMatcher: attributeSelectorWithMatcherVisitor });
+    traverser.visit(AST); 
+    const { calls } = attributeSelectorWithMatcherVisitor.mock;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toBe(attributeSelectorWithMatcher1);
+  });
+
+  it('calls `ClassSelector` for each `ClassSelector`-node', () => {
+    const classSelectorVisitor = jest.fn();
+    const traverser = new Traverser({ ClassSelector: classSelectorVisitor });
+    traverser.visit(AST); 
+    const { calls } = classSelectorVisitor.mock;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toBe(classSelector1);
+  });
+
+  it('calls `Combinator` for each `Combinator`-node', () => {
+    const combinatorVisitor = jest.fn();
+    const traverser = new Traverser({ Combinator: combinatorVisitor });
+    traverser.visit(AST); 
+    const { calls } = combinatorVisitor.mock;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toBe(combinator1);
+  });
+
+  it('calls `HashSelector` for each `HashSelector`-node', () => {
+    const hashSelectorVisitor = jest.fn();
+    const traverser = new Traverser({ HashSelector: hashSelectorVisitor });
+    traverser.visit(AST); 
+    const { calls } = hashSelectorVisitor.mock;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toBe(hashSelector1);
+  });
+
+  it('calls `Identifier` for each `Identifier`-node', () => {
+    const identifierVisitor = jest.fn();
+    const traverser = new Traverser({ Identifier: identifierVisitor });
+    traverser.visit(AST); 
+    const { calls } = identifierVisitor.mock;
+    expect(calls.length).toBe(11);
+    expect(calls[0][0]).toBe(hashSelector1.value);
+    expect(calls[1][0]).toBe(classSelector1.value);
+    expect(calls[2][0]).toBe(attributeSelector1.attribute.value);
+    expect(calls[3][0]).toBe(attributeSelectorWithMatcher1.attribute.value);
+    expect(calls[4][0]).toBe(pseudoClassSelector1.value);
+    expect(calls[5][0]).toBe(pseudoElementSelector1.value);
+    expect(calls[6][0]).toBe(typeSelector1.value);
+    expect(calls[7][0]).toBe(namespacePrefix1.value);
+    expect(calls[8][0]).toBe(callExpression1.callee);
+    expect(calls[9][0]).toBe(callExpression2.callee);
+    expect(calls[10][0]).toBe(typeSelector2.value);
+  });
+
+  it('calls `Literal` for each `Literal`-node', () => {
+    const literalVisitor = jest.fn();
+    const traverser = new Traverser({ Literal: literalVisitor });
+    traverser.visit(AST); 
+    const { calls } = literalVisitor.mock;
+    expect(calls.length).toBe(3);
+    expect(calls[0][0]).toBe(attributeSelectorWithMatcher1.value.value);
+    expect(calls[1][0]).toBe(nthChildExpression1.step);
+    expect(calls[2][0]).toBe(nthChildExpression1.offset);
+  });
+
+  it('calls `NamespacePrefix` for each `NamespacePrefix`-node', () => {
+    const namespacePrefixVisitor = jest.fn();
+    const traverser = new Traverser({ NamespacePrefix: namespacePrefixVisitor });
+    traverser.visit(AST); 
+    const { calls } = namespacePrefixVisitor.mock;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toBe(namespacePrefix1);
+  });
+
+  it('calls `NumberLiteral` for each `NumberLiteral`-node', () => {
+    const numberLiteralVisitor = jest.fn();
+    const traverser = new Traverser({ NumberLiteral: numberLiteralVisitor });
+    traverser.visit(AST); 
+    const { calls } = numberLiteralVisitor.mock;
+    expect(calls.length).toBe(2);
+    expect(calls[0][0]).toBe(nthChildExpression1.step);
+    expect(calls[1][0]).toBe(nthChildExpression1.offset);
+  });
+
+  it('calls `PseudoSelector` for each `PseudoSelector`-node', () => {
+    const pseudoSelectorVisitor = jest.fn();
+    const traverser = new Traverser({ PseudoSelector: pseudoSelectorVisitor });
+    traverser.visit(AST); 
+    const { calls } = pseudoSelectorVisitor.mock;
+    expect(calls.length).toBe(4);
+    expect(calls[0][0]).toBe(pseudoClassSelector1);
+    expect(calls[1][0]).toBe(pseudoElementSelector1);
+    expect(calls[2][0]).toBe(pseudoClassSelector2);
+    expect(calls[3][0]).toBe(pseudoClassSelector3);
+  });
+
+  it('calls `CallExpression` for each `CallExpression`-node', () => {
+    const callExpressionVisitor = jest.fn();
+    const traverser = new Traverser({ CallExpression: callExpressionVisitor });
+    traverser.visit(AST); 
+    const { calls } = callExpressionVisitor.mock;
+    expect(calls.length).toBe(2);
+    expect(calls[0][0]).toBe(callExpression1);
+    expect(calls[1][0]).toBe(callExpression2);
+  });
+
+  it('calls `PseudoClassSelector` for each `PseudoClassSelector`-node', () => {
+    const pseudoClassSelectorVisitor = jest.fn();
+    const traverser = new Traverser({ PseudoClassSelector: pseudoClassSelectorVisitor });
+    traverser.visit(AST); 
+    const { calls } = pseudoClassSelectorVisitor.mock;
+    expect(calls.length).toBe(3);
+    expect(calls[0][0]).toBe(pseudoClassSelector1);
+    expect(calls[1][0]).toBe(pseudoClassSelector2);
+    expect(calls[2][0]).toBe(pseudoClassSelector3);
+  });
+
+  it('calls `PseudoElementSelector` for each `PseudoElementSelector`-node', () => {
+    const pseudoElementSelectorVisitor = jest.fn();
+    const traverser = new Traverser({ PseudoElementSelector: pseudoElementSelectorVisitor });
+    traverser.visit(AST); 
+    const { calls } = pseudoElementSelectorVisitor.mock;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toBe(pseudoElementSelector1);
+  });
+
+  it('calls `Selector` for each `Selector`-node', () => {
+    const selectorVisitor = jest.fn();
+    const traverser = new Traverser({ Selector: selectorVisitor });
+    traverser.visit(AST); 
+    const { calls } = selectorVisitor.mock;
+    expect(calls.length).toBe(3);
+    expect(calls[0][0]).toBe(selector1);
+    expect(calls[1][0]).toBe(selector2);
+    expect(calls[2][0]).toBe(selector3);
+  });
+
+  it('calls `SelectorList` for each `SelectorList`-node', () => {
+    const selectorListVisitor = jest.fn();
+    const traverser = new Traverser({ SelectorList: selectorListVisitor });
+    traverser.visit(AST); 
+    const { calls } = selectorListVisitor.mock;
+    expect(calls.length).toBe(2);
+    expect(calls[0][0]).toBe(selectorList1);
+    expect(calls[1][0]).toBe(selectorList2);
+  });
+
+  it('calls `SimpleSelector` for each `SimpleSelector`-node', () => {
+    const simpleSelectorVisitor = jest.fn();
+    const traverser = new Traverser({ SimpleSelector: simpleSelectorVisitor });
+    traverser.visit(AST); 
+    const { calls } = simpleSelectorVisitor.mock;
+    expect(calls.length).toBe(12);
+    expect(calls[0][0]).toBe(universalSelector1);
+    expect(calls[1][0]).toBe(hashSelector1);
+    expect(calls[2][0]).toBe(classSelector1);
+    expect(calls[3][0]).toBe(attributeSelector1);
+    expect(calls[4][0]).toBe(attributeSelectorWithMatcher1);
+    expect(calls[5][0]).toBe(pseudoClassSelector1);
+    expect(calls[6][0]).toBe(pseudoElementSelector1);
+    expect(calls[7][0]).toBe(typeSelector1);
+    expect(calls[8][0]).toBe(pseudoClassSelector2);
+    expect(calls[9][0]).toBe(pseudoClassSelector3);
+    expect(calls[10][0]).toBe(universalSelector2);
+    expect(calls[11][0]).toBe(typeSelector2);
+  });
+
+  it('calls `SimpleSelectorList` for each `SimpleSelectorList`-node', () => {
+    const simpleSelectorListVisitor = jest.fn();
+    const traverser = new Traverser({ SimpleSelectorList: simpleSelectorListVisitor });
+    traverser.visit(AST); 
+    const { calls } = simpleSelectorListVisitor.mock;
+    expect(calls.length).toBe(3);
+    expect(calls[0][0]).toBe(simpleSelectorList1);
+    expect(calls[1][0]).toBe(simpleSelectorList2);
+    expect(calls[2][0]).toBe(simpleSelectorList3);
+  });
+
+  it('calls `StringLiteral` for each `StringLiteral`-node', () => {
+    const stringLiteralVisitor = jest.fn();
+    const traverser = new Traverser({ StringLiteral: stringLiteralVisitor });
+    traverser.visit(AST); 
+    const { calls } = stringLiteralVisitor.mock;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toBe(attributeSelectorWithMatcher1.value.value);
+  });
+
+  it('calls `TypeSelector` for each `TypeSelector`-node', () => {
+    const typeSelectorVisitor = jest.fn();
+    const traverser = new Traverser({ TypeSelector: typeSelectorVisitor });
+    traverser.visit(AST); 
+    const { calls } = typeSelectorVisitor.mock;
+    expect(calls.length).toBe(2);
+    expect(calls[0][0]).toBe(typeSelector1);
+    expect(calls[1][0]).toBe(typeSelector2);
+  });
+
+  it('calls `UniversalSelector` for each `UniversalSelector`-node', () => {
+    const universalSelectorVisitor = jest.fn();
+    const traverser = new Traverser({ UniversalSelector: universalSelectorVisitor });
+    traverser.visit(AST); 
+    const { calls } = universalSelectorVisitor.mock;
+    expect(calls.length).toBe(2);
+    expect(calls[0][0]).toBe(universalSelector1);
+    expect(calls[1][0]).toBe(universalSelector2);
+  });
 });
